31e2080b14bcd043ad7f149201222a4bdd2dc542
Janak <hdabhi902@rku.ac.in>
2023-12-14 20:53:09 +0530

    Add js

--- restaurant.js
+++ restaurant.js
@@ -1,2 +1,21 @@
 console.log('Console');
 
+function displayTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0'); // Get the current hour
+    const minutes = String(now.getMinutes()).padStart(2, '0'); // Get the current minute
+    const seconds = String(now.getSeconds()).padStart(2, '0'); // Get the current second
+    
+    const currentTime = `${hours}:${minutes}:${seconds}`; // Format the time as HH:MM:SS
+    
+    document.getElementById('time').textContent = currentTime; // Display the time in the 'time' paragraph
+    
+    setTimeout(displayTime, 1000); // Update the time every second
+  }
+
+  let h4=document.body.children[1].children[0].children[2];
+  console.log(h4);
+
+  h4.innerText=`<b>${displayTime()}</b>`;
+
+  
